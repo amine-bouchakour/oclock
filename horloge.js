a38c19a8e8a439d81d3e8699ede506b8fc62e72f
Bouchakour <amine.bouchakour@laplateforme.io>
2020-03-16 19:50:49 +0100

    Horloge fonctionnel

--- horloge.js
+++ horloge.js
@@ -1,39 +1,19 @@
-$( document ).ready(function() {
+function appel(){
+    
+    var today =new Date();
+    var h=today.getHours();
+    var mn=today.getMinutes();
+    var s=today.getSeconds();
+    
+    h=(h>9? h:"0" + h);
+    mn=(mn>9? mn:"0" + mn);
+    s=(s>9? s:"0" + s);
 
+    var heure = h + ':' + mn + ":" + s;
 
+    document.getElementById('Hour').innerHTML = heure;
+    reload = setTimeout("appel()",1000);
+}
 
+appel();
 
-
-
-
-
-});
-
-// //variable qui permet d'identifier le timer afin de pouvoir l'arrêter
-// var timerID = null;
-// var timerActif = false;
-
-// function stopClock() {
-//   if (timerActif) clearTimeout(timerID);
-//   timerActif = false;
-// }
-
-// function startClock() {
-//   stopClock();
-//   showtime();
-// }
-
-// function showtime() {
-//   var now = new Date();
-//   var hour = now.getHours();
-//   var min = now.getMinutes();
-//   var sec = now.getSeconds();
-//   heure = (hour > 9? hour:"0" + hour);
-//   heure += ":" + (min > 9? min:"0" + min);
-//   heure += ":" + (sec > 9? sec:"0" + sec);
-//   document.Clock.Horloge.value = heure;
-//   //pour mettre l'horloge dans la barre de status :
-//   //window.status = heure;
-//   timerID = setTimeout("showtime()",1000);
-//   timerActif = true;
-// }
