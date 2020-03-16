522646b112885ea07e66bf21c95249b221a7dcf4
Bouchakour <amine.bouchakour@laplateforme.io>
2020-03-16 22:29:06 +0100

    creation fichier index en php au cas ou

---
+++ oclock.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Occlock</title>
+    <link rel="stylesheet" href="oclock.css">
+    <script src="Jquery" refer></script>
+  
+</head>
+<body>
+
+  <?php
+$toto="bonjour";
+  ?>
+    
+<form id="Oclock">
+  <p id=Hour></p>
+</form>
+
+<!-- <input type="button" id="more_com" value="valider"> -->
+
+
+<script src="horloge.js"></script>
+<script src="reveil.js"></script>
+</body>
+</html>
